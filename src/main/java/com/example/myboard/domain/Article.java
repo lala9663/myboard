52b8b94d458a3dab2f8fb3ae33c7d19c94832033
lala9663 <lala96632040@gmail.com>
2024-02-02 17:59:51 +0900

    #1-data rest 설정
    게시글 댓글 api

--- src/main/java/com/example/myboard/domain/Article.java
+++ src/main/java/com/example/myboard/domain/Article.java
@@ -18,7 +18,7 @@ import java.util.Set;
         @Index(columnList = "createdBy")
 })
 @Entity
-public class Article extends AuditingFields {
+public class Article extends AuditingFields  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/example/myboard/repository/ArticleRepository.java
+++ src/main/java/com/example/myboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.example.myboard.repository;
 
 import com.example.myboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
