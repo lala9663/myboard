4b129950e9dd048497d34c93b115954f30c363f9
lala9663 <lala96632040@gmail.com>
2024-02-05 17:35:44 +0900

    #9-repository 변경

--- src/main/java/com/example/myboard/repository/ArticleRepository.java
+++ src/main/java/com/example/myboard/repository/ArticleRepository.java
@@ -1,20 +1,28 @@
 package com.example.myboard.repository;
 
 import com.example.myboard.domain.Article;
+import com.example.myboard.domain.ArticleComment;
 import com.example.myboard.domain.QArticle;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         QuerydslPredicateExecutor<Article>, //기본적으로 이 entity에 있는 모든 검색 기능
         QuerydslBinderCustomizer<QArticle> {
+
+    Page<Article> findByTitle(String title, Pageable pageable);
+
 
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
