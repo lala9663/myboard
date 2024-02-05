2865b83dc64ca964cfbd3606148859a5b1034cce
lala9663 <lala96632040@gmail.com>
2024-02-05 17:37:14 +0900

    #9-service 변경
    유저 관련 추가로 인해 코드 변경 및 추가 작성

---
+++ src/main/java/com/example/myboard/service/ArticleService.java
@@ -0,0 +1,42 @@
+package com.example.myboard.service;
+
+import com.example.myboard.domain.type.SearchType;
+import com.example.myboard.dto.ArticleDto;
+import com.example.myboard.dto.ArticleUpdateDto;
+import com.example.myboard.dto.ArticleWithCommentsDto;
+import com.example.myboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleWithCommentsDto getArticle(Long articleId) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(ArticleDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+
+}
