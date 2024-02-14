aedd74d1e87ee3ea74151fd8e910cb0b49908311
lala9663 <lala96632040@gmail.com>
2024-02-15 01:45:50 +0900

    #10-pagination

--- src/main/java/com/example/myboard/controller/ArticleController.java
+++ src/main/java/com/example/myboard/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.example.myboard.domain.type.SearchType;
 import com.example.myboard.response.ArticleResponse;
 import com.example.myboard.response.ArticleWithCommentsResponse;
 import com.example.myboard.service.ArticleService;
+import com.example.myboard.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,12 +17,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
+
     @GetMapping
     public String articles(
             @RequestParam(required = false) SearchType searchType,
@@ -29,6 +35,11 @@ public class ArticleController {
             ModelMap map
       ) {
         map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumber(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
 
         return "articles/index";
     }
